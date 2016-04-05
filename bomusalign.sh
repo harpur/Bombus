6b3a79084053b4d0c76a9d20f9162eb857ff13b2
harpur <brock@bombus.biol.yorku.ca>
2016-04-05 17:27:56 -0400

    README

--- bomusalign.sh
+++ bomusalign.sh
@@ -4,26 +4,15 @@
 
 
 
-#declare environment variables -------------------------------
-export BOMBUS=/media/data2/bombus/
-
-
-			export BAM=/media/data1/afz/BAM
-			export MASK=
-			export VCF=/media/data1/afz/VCF
-			export AFZ=/media/data1/afz
-			export GIT=/media/data1/git
-			export FASTQ=/media/data1/fastq
-			export REF=/media/data1/fastq
-
-
-			/media/data1/bombus/BimpFullGenome.fa
-
+#Environment variables -------------------------------
+export BOMBUS=/media/data2/bombus
+export MASK=/media/data1/bombus/MaskedSNPs
+export SNPEFF=/usr/local/lib/snpEff2
 
 
 # Call SNPs with GATK -----------------------------------
 	#All 
-ls *.bam > bams.list 
+ls *.bam > bams.list  #assemble list of bams in species directory
 
 #Call SNPs
 gatk -R $BOMBUS/BimpFullGenome.fa -T UnifiedGenotyper \
@@ -44,7 +33,7 @@ gatk -R $BOMBUS/BimpFullGenome.fa -T UnifiedGenotyper \
 	-stand_emit_conf 40.0 \
 	-dcov 200 \
 	--min_base_quality_score 20  \
-	-nt 20 -glm SNP  \
+	-nt 20 -glm indel  \
 	-ploidy 1 &
  
 
@@ -56,138 +45,62 @@ gatk -R $BOMBUS/BimpFullGenome.fa -T UnifiedGenotyper \
 	-stand_emit_conf 40.0 \
 	-dcov 200 \
 	--min_base_quality_score 20  \
-	-nt 30 -glm SNP  \
-	-ploidy 2 &
- 
-  
- #BETR
- #removed 88 and 89, they are too low quality and highly hetero
-	#-I Bter-88_001.sorted.gatk.bam -I Bter-89_001.sorted.gatk.bam
-  
- 
- 
- 
- 
- 
+	-nt 20 -glm SNP  \
+	-ploidy 2
+	
+#Output hetero SNPs --------------------
+vcftools --vcf out.het.raw.vcf --hardy #2792802 out.hwe
+sed '/[/]0[/]/ d' out.hwe > test.out
+sed '/[/]1[/]/ d' test.out > test1.out #447528SNPs 
+cut -f 1,2 test1.out > putativeCNV.list 
+rm test.out
+rm test1.out
+grep -Fwf putativeCNV.list out.raw.vcf > cnv.vcf
+sed '/^#/ !d' out.raw.vcf > VCFheader
+cat VCFheader cnv.vcf > cnv1.vcf
+rm cnv.vcf
+mv cnv1.vcf cnv.vcf
 
-##
-#Make first filtered file (RAW) called Bter.raw.recode.vcf
-#gatk -R /media/data1/bombus/BterrFullGenome.fa -T VariantFiltration -V  Bter.raw1.vcf --filterExpression "QD < 5.0 || FS > 40.0 || MQ < 25.0 || DP < 100.0"   --filterName "mpileFilters" -o  Bter.raw2.vcf
-#
-vcftools --vcf Bter.raw2.vcf --recode --remove-filtered-all --out Bter.raw
+# Filter Variants  -----------------------------------
+gatk -R $BOMBUS/BimpFullGenome.fa -T VariantFiltration \
+	-V out.raw.vcf \
+	--filterExpression "QD < 5.0 || FS > 40.0 || MQ < 25.0 "   \
+	--filterName "mpileFilters" \
+	--mask out.raw.indels.vcf  \
+	--maskExtension 10 \
+	--maskName "InDel" -o  out.vcf 
 
+gatk -R $BOMBUS/BimpFullGenome.fa -T VariantFiltration \
+	-V out.vcf \
+	--mask cnv.vcf \
+	--maskExtension 5 \
+	--maskName "cnv" \
+	-o  out2.vcf
+rm out.vcf 
+mv out2.vcf out.vcf #2488716
 
+vcftools --vcf out.vcf --recode --remove-filtered-all --max-alleles 2 --out out.indel #will be called out.indel.recode.vcf
+#wc -l 	out.indel.recode.vcf 2192172 (88% of original SNPs in BIMP)
 
-##
-#Make second Filtered File (INDELS) Bter.indel.recode.vcf
-gatk -R /media/data1/bombus/BterrFullGenome.fa -T VariantFiltration -V  Bter.raw.recode.vcf -o  Bter.indel.vcf --mask Bter.raw.indel.vcf --maskExtension 10 --maskName "InDel" 
+# Filter Depth and Quality outliers  -----------------------------------
+VCFQualityDepthFilter.r "out.indel.recode.vcf" 
 
-vcftools --vcf Bter.indel.vcf --recode --remove-filtered-all --out Bter.indel
-
-
-##
-#Make third file (depth), called Bter.indel.dp.recode.vcf
-vcftools --vcf Bimp.indel.recode.vcf --site-mean-depth
-#Coded in a separate Rscript, but here is some of the code within:
-R
-depth=read.table(file="out.ldepth.mean",header=T)
-maxdp = 1.5*IQR(depth$MEAN_DEPTH)+quantile(depth$MEAN_DEPTH,0.75) #maxdp
-mindp = quantile(depth$MEAN_DEPTH,0.25)-1.5*IQR(depth$MEAN_DEPTH) #mindp
-d1 = depth[depth$MEAN_DEPTH <= mindp,]
-d2 = rbind(d1, depth[depth$MEAN_DEPTH >= maxdp,])
-write.table(d2[c(1,2)], file="BIMPDepthSNPs",col.names=F,row.names=F,quote=F)
-
-vcftools --vcf Bimp.indel.dp.recode.vcf  --exclude-positions BIMPDepthSNPs --recode --remove-filtered-all  --out Bter.indel.dp
-
-
-##
-#Make forth file (BLAST filter) Bter.indel.dp.recode.vcf
-vcftools --vcf Bimp.indel.dp.recode.vcf  --exclude-positions /media/data1/bombus/MaskedSNPs/BimpAllSNPs --recode --remove-filtered-all  --out Bimp.indel.dp.bl
-
-
-
-##
-#Make forth file (diploid drone calls) Bter.indel.dp.bl.dd.recode.vcf
-	#Bter.raw.DIPLOID.vcf contains a set of SNOs called when diploid
-	#Had to remove 88 and 89
-	#vcftools --vcf Bter.raw.DIPLOID.vcf  --remove-indv "Bter-89" --remove-indv "Bter-88" --recode
-
-vcftools --vcf Bter.raw.DIPLOID.vcf  --hardy 
-
-R
-hwe=read.table(file="out.hwe",header=T)
-hets=apply(hwe[3],1,function(x) unlist(strsplit(x, "/"))[2])
-hwe1=hwe[c(1,2)][which(hets>0),]
-write.table(file="/media/data1/bombus/MaskedSNPs/DiploidBterr",hwe1,col.names=F,row.names=F,quote=F)
+# Calling synonymous and non-synonymous sites -------------------------
+	#note, v4.0 of SNPEFF no longer supports TXT format (I think)...annoying.
+java -jar $SNPEFF/snpEff.jar Bimp \
+	-o txt \
+	out.indel.dp.q.recode.vcf \
+	-no-downstream \
+	-no-upstream > out.snpeff.eff
+	
+sed '/SYNONYMOUS/ !d' out.snpeff.eff > exons.eff #take only SNPS within exons from output file
+sed '/WARNING/ !d' exons.eff > warnexons.eff #take out warnings from output file
+sed -i '/WARNING/ d' exons.eff 
 
 
-vcftools --vcf Bimp.indel.dp.bl.recode.vcf   --exclude-positions /media/data1/bombus/MaskedSNPs/DiploidBimp --recode --remove-filtered-all --out Bimp.indel.dp.bl.dd
-
-
-
-
-#There it is, I get five files:
-	#Bter.raw.recode.vcf
-	#Bter.indel.recode.vcf
-	#Bter.indel.dp.recode.vcf
-	#Bter.indel.dp.recode.vcf
-	#Bter.indel.dp.bl.dd.recode.vcf
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#Calling NSYN and SYN SNPs:
-#http://snpeff.sourceforge.net/SnpEff_manual.html
-	#search "databases"
-	#Update the .config file 
-
-java -jar /usr/local/lib/snpEff2/snpEff.jar build -gff3 -v Bterr
-
-
-#Run SNPEff
-	#Run on AfrSNPs.raw.vcf, it contains all SNPs 
-java -jar /usr/local/lib/snpEff2/snpEff.jar Bterr -o txt Bter.raw.recode.vcf  -no-downstream -no-upstream  > Bterr.snpeff.eff
-
-	#Will NEED to remove genes with premature stop codons and any trimmed genes in Daria's analyses
-
-sed '/SYNONYMOUS/ !d' Bterr.snpeff.eff > exons.eff
-sed '/WARNING/ !d' exons.eff > warnexons.eff
-sed '/WARNING/ d' exons.eff > exons1.eff #renamed exons.eff
-
-
-
-#Make a vcf file of ONLY the NSYN SNPs and ONLY the SYN SNPs:
-	#wrote out NSYN.snp and SYN.snp from R using exons1.eff
-	nsyn=read.table(file="/media/data1/forty3/brock/align/exons.eff")
-	nsyn=nsyn[,-c(5:7,9,10,11,12,16,17)]
-	nsyn$nsyn=gsub("_.*","",nsyn$V13)
-	nsyn$V13=NULL
-	syn=nsyn[nsyn$nsyn!="NON",]
-	write.table(syn[c(1,2)],file="SYN.snp",quote=F,col.names=F,row.names=F)
-	nsyn=nsyn[nsyn$nsyn=="NON",]
-	write.table(nsyn[c(1,2)],file="NSYN.snp",quote=F,col.names=F,row.names=F)
-
-	nsyn=read.table(file="/media/data1/forty3/brock/align/warnexons.eff")
-	nsyn=nsyn[,-c(5:8,10,11,12,13,16,17,18)]
-	nsyn$nsyn=gsub("_.*","",nsyn$V14)
-	syn=nsyn[nsyn$nsyn!="NON",]
-	write.table(syn[c(1,2)],file="SYN.snp",quote=F,col.names=F,row.names=F,append=T)
-	nsyn=nsyn[nsyn$nsyn=="NON",]
-	write.table(nsyn[c(1,2)],file="NSYN.snp",quote=F,col.names=F,row.names=F,append=T)
-
-
+# make VCF file of only SYN and NSYN SNPs -------------------------
+cut -f 1,2 exons.eff  > synnsyn.list 
+vcftools --vcf out.indel.dp.q.recode.vcf --positions synnsyn.list --recode --out out.nsyn
 
 
 
